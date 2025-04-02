c72c7c50f2f2dd73cd46e8e30a36610ac742c6f9
Matheus Felipe G. Ferreira <mateusfelipeg.f@gmail.com>
2025-04-01 21:00:50 -0300

    Add files via upload

---
+++ EstudanteMain.java
@@ -0,0 +1,9 @@
+package Estudante;
+
+public class Main {
+    public static void main(String[] args) {
+        Estudante Estudante = new Estudante("MATHEUS", "442.524.563-85", "ADS", 18);
+        Estudante.exibirCadastro();
+
+    }
+}
